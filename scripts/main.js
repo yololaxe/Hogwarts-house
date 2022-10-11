5b33c5ae08ba4fb8c4cba77226e256686eb46ffa
yololaxe <yololaxe971@gmail.com>
2022-10-11 22:05:06 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 
 let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1')
+let myHeading = document.querySelector('h1');
 
 function addPoint(num){
     myHeading.innerHTML = '' + num;
